89bdd25e42e844560ff6b579a02eb83ef175f260
2430443-cmd <2430443@upt.edu.mx>
2025-11-03 11:07:38 -0600

    Update build.gradle.kts
    configuracion del proyecto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//Archivo de compilación de nivel superior donde puede agregar opciones de configuración comunes a todos los subproyectos/módulos.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
